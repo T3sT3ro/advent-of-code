ec6e2ac1af09d8b4020764afe44438f1dbededec
Maksymilian Polarczyk <maksymilian.polarczyk@adtran.com>
2024-12-13 20:16:36 +0100

    add 2024/07.2 and test for sanity

--- 2024/07.mjs
+++ 2024/07.mjs
@@ -11,10 +11,32 @@ const t = $('IN/07').textContent.trim()
 
 /**
  * 
- * @param {[test: number, numbers: number[]]} param0 
+ * @param {number} test 
+ * @param {number[]} numbers 
+ * @param {number} i 
+ * @param {boolean} withConcat
+ * @returns 
  */
-function checkValidity([test, numbers]) {
+function checkValidity(test, numbers, i, withConcat) {
+    const last = numbers[i];
+    if (i == 0 && last == test) return true;
+    if (test % last == 0 && checkValidity(test / last, numbers, i - 1, withConcat)) return true;
+    if (test - last >= 0 && checkValidity(test - last, numbers, i - 1, withConcat)) return true;
+    if (withConcat && `${test}`.endsWith(`${last}`)
+        && checkValidity(+`${test}`.slice(0, -`${last}`.length), numbers, i - 1, withConcat)
+    ) return true;
 
+    return false;
 }
 
-console.log(t.filter(checkValidity).reduce((a, [test, numbers]) => a + test))
+console.log(
+    t.filter(([test, numbers]) =>
+        checkValidity(test, numbers, numbers.length - 1, false)
+    ).reduce((a, [test, numbers]) => a + test, 0)
+);
+
+console.log(
+    t.filter(([test, numbers]) =>
+        checkValidity(test, numbers, numbers.length - 1, true)
+    ).reduce((a, [test, numbers]) => a + test, 0)
+);
