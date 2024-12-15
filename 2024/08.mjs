16cd84f17b5eb1d33f5feca2be11b58301a3d745
Maksymilian Polarczyk <maksymilian.polarczyk@adtran.com>
2024-12-15 14:43:15 +0100

    add 2024/08.2

--- 2024/08.mjs
+++ 2024/08.mjs
@@ -13,37 +13,35 @@ const ants = _([...iter2D(t)])
     .mapValues(ex => ex.map(({ rc }) => rc))
     .value();
 
-/** @type {{[K in keyof ants]: [number, number][]}} */
-const antinodes = {};
-/** @type {{[rc: string]: [keyof ants][]}} */
+/** @type {{[rc: string]: boolean}} */
 const locations = {};
+/** @type {{[rc: string]: boolean}} */
+const locations2 = {};
+
 for (const [freq, rcx] of Object.entries(ants)) {
-    antinodes[freq] = [];
     for (let i = 0; i < rcx.length - 1; i++) {
         for (let j = i + 1; j < rcx.length; j++) {
             const [x1, y1] = rcx[i];
             const [x2, y2] = rcx[j];
             let delta = [x2 - x1, y2 - y1];
-            const a1 = [x2 + delta[0], y2 + delta[1]];
-            const a2 = [x1 - delta[0], y1 - delta[1]];
-            if (inBounds(a1)) {
-                locations[a1] = [...(locations[a1] || []), freq];
-                antinodes[freq].push(a1);
+
+            function* nextDir(rc, dir, k) {
+                while (inBounds(rc)) {
+                    yield { rc: rc, dir: dir, k: k};
+                    rc = [rc[0] + dir[0], rc[1] + dir[1]];
+                    ++k;
+                }
             }
 
-            if (inBounds(a2)) {
-                locations[a2] = [...(locations[a2] || []), freq];
-                antinodes[freq].push(a2);
+            for (const {rc, dir, k} of [...nextDir(rcx[i], delta, 0), ...nextDir(rcx[j], delta.map(x => -x), 0)]){
+                if(k==2) locations[rc] = true;
+                locations2[rc] = true;
             }
-            // reference: https://pl.wikipedia.org/wiki/Prosta
-            // const A = y2 - y1;
-            // const B = x1 - x2;
-            // const C = x2 * y1 - x1 * y2;
         }
     }
 }
 
 function inBounds([r, c]) { return r >= 0 && r < t.length && c >= 0 && c < t[0].length; }
 
-console.log(_.uniq(Object.values(antinodes).flat().filter(inBounds).map(rc => rc.toString())));
 console.log(Object.keys(locations).length);
+console.log(Object.keys(locations2).length);
