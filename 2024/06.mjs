376fa56ec0267fa973bbdb10617a28e3f1399eae
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-12-01 23:56:48 +0100

    added 2024/06.2 (brute force)

--- 2024/06.mjs
+++ 2024/06.mjs
@@ -8,23 +8,69 @@ const t = $('IN/06').textContent.trim()
 
 import { find2D, iter2D, map2D } from '../util/src/util.mjs';
 
-let guard = find2D(t, ({e, rc}) => e == '^').rc;
-let walls = Object.fromEntries(iter2D(t).filter(({e}) => e == '#').map(({rc}) => [rc, true]));
+let guard = find2D(t, ({ e, rc }) => e == '^').rc;
+let walls = Object.fromEntries(iter2D(t).filter(({ e }) => e == '#').map(({ rc }) => [rc, true]));
 
 const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let dir = 0;
+function findPath(guard, walls) {
+    let dir = 0;
 
-const seen = {};
-function outOfBounds(rc) { return rc[0] < 0 || rc[0] >= t.length || rc[1] < 0 || rc[1] >= t[0].length; }
+    const seen = {};
+    function outOfBounds(rc) {
+        return rc[0] < 0 || rc[0] >= t.length || rc[1] < 0 || rc[1] >= t[0].length;
+    }
 
-let state = [guard, dir];
-while(!seen[state] && !outOfBounds(guard)) {
-    seen[state] = true;
-    while(walls[[guard[0]+dirs[dir][0], guard[1]+dirs[dir][1]]])
-        dir = (dir+1)%4;
+    let state = [guard, dir];
+    while (!seen[state] && !outOfBounds(guard)) {
+        seen[state] = true;
+        while (walls[[guard[0] + dirs[dir][0], guard[1] + dirs[dir][1]]])
+            dir = (dir + 1) % 4;
 
-    guard = [guard[0]+dirs[dir][0], guard[1]+dirs[dir][1]];
-    state = [guard, dir];
+        guard = [guard[0] + dirs[dir][0], guard[1] + dirs[dir][1]];
+        state = [guard, dir];
+    }
+
+    return _.uniq(
+        Object.keys(seen)
+            .map(k => k.split(','))
+            .map(([r, c, dir]) => `${r},${c}`))
+        .length;
 }
 
-console.log(_.uniq(Object.keys(seen).map(k => k.split(',')).map(([r, c, dir]) => `${r},${c}`)).length);
+
+console.log(findPath(guard, walls));
+
+function isLooping (guard, walls) {
+    let dir = 0;
+
+    const seen = {};
+    function outOfBounds(rc) {
+        return rc[0] < 0 || rc[0] >= t.length || rc[1] < 0 || rc[1] >= t[0].length;
+    }
+
+    let state = [guard, dir];
+    while (!seen[state]) {
+        if(outOfBounds(guard)) return false;
+        seen[state] = true;
+        while (walls[[guard[0] + dirs[dir][0], guard[1] + dirs[dir][1]]])
+            dir = (dir + 1) % 4;
+
+        guard = [guard[0] + dirs[dir][0], guard[1] + dirs[dir][1]];
+        state = [guard, dir];
+    }
+
+    return true;
+}
+
+// fuck it, go full brute
+let positionsWhichLoop = 0;
+for (let i = 0; i < t.length; i++) {
+    for (let j = 0; j < t[0].length; j++) {
+        if(t[i][j] == '.' && isLooping(guard, {...walls, [[i, j]]: true})){
+            console.warn(i, j);
+            positionsWhichLoop++;
+        }
+    }
+}
+
+console.log(positionsWhichLoop);
