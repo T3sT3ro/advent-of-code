8bd759623a5213866a79909668b132c378a5dda1
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-12-01 23:11:56 +0100

    Add AoC 2025/01

---
+++ 2025/01.ts
@@ -0,0 +1,27 @@
+import $ from '../in.mjs';
+import { sum, windowed } from 'es-toolkit';
+import 'core-js/es/object/group-by.js';
+import 'core-js/actual/iterator/index.js';
+// --- browser devtools cutoff ---
+const t = $('IN/01').textContent.trim()
+    .split('\n');
+
+const d = t.map(tx => [tx[0], tx.slice(1)])
+    .map(([dir, num]) => (dir == 'L' ? -1 : 1) * +num);
+
+const SIZE = 100;
+let pos = 50;
+let [atZero, throughZero] = [0, 0];
+
+for (const delta of d) {
+    let deltaReminder = delta % SIZE;
+    const newPos = pos + deltaReminder;
+    throughZero += Math.floor(Math.abs(delta) / SIZE);
+    if (pos != 0 && (newPos >= SIZE || newPos <= 0))
+        ++throughZero;
+    pos = (newPos + SIZE) % SIZE;
+    if (pos == 0)
+        ++atZero;
+}
+
+console.log(atZero, throughZero);
