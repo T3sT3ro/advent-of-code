e8d0c0fa3e9e4584d883f286b42e487041215209
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-12-04 11:43:56 +0100

    Change default TS template due to indentation pain point

--- templates/default.ts
+++ templates/default.ts
@@ -3,5 +3,5 @@ import 'es-toolkit';
 // import _ from 'lodash';
 import 'core-js/actual/iterator/index.js';
 // --- browser devtools cutoff ---
-const t = $('IN/<<<DAY>>>').textContent.trim()
-    .split('\n');
+const t = $('IN/<<<DAY>>>').textContent.trim().split('\n');
+
