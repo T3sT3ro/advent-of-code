00235818af8e0b123945ca0bbbc5016d3c4b3592
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-12-09 00:37:42 +0100

    Improve 25/05

--- 2025/05.ts
+++ 2025/05.ts
@@ -8,36 +8,31 @@ const t = $('IN/05').textContent.trim().split('\n\n');
 const ranges = t[0].split('\n').map(r => r.split('-').map(Number));
 const ids = t[1].split('\n').map(Number);
 
-let a1 = 0;
-nextId:
-for (const id of ids) {
-    for (const [s, e] of ranges) {
-        if (s <= id && id <= e) {
-            a1++;
-            continue nextId;
-        }
-    }
-}
+type event = { time: number, type: 'start' | 'end' | 'point' };
 
-console.log(a1);
-
-let events = [];
+let events: event[] = ids.map(id => ({ time: id, type: 'point' }));
 for (const [s, e] of ranges) {
     events.push({ time: s, type: 'start' });
     events.push({ time: e + 1, type: 'end' });
 }
+const eventToOrder = { 'start': 0, 'point': 1, 'end': 2 };
+events.sort((a, b) => a.time - b.time || eventToOrder[a.type] - eventToOrder[b.type]);
 
-events.sort((a, b) => a.time - b.time || (a.type === 'start' ? -1 : 1));
+const rangeDelta = { 'start': 1, 'end': -1, 'point': 0 };
 
 let total = 0
-let lastStart = events.shift()!;
-let currentRanges = 1;
+let fresh = 0;
+let start = 0;
+let currentRanges = 0;
 for (const e of events) {
-    if(currentRanges === 0) 
-        lastStart = e;
-    currentRanges += (e.type === 'start' ? 1 : -1);
-    if (currentRanges === 0)
-        total += e.time - lastStart.time;
+    currentRanges += rangeDelta[e.type];
+    if (e.type === 'point' && currentRanges > 0) {
+        ++fresh;
+    } else if (e.type === 'start' && currentRanges === 1) {
+        start = e.time;
+    } else if(e.type === 'end' && currentRanges === 0) {
+        total += e.time - start;
+    }
 }
 
-console.log(total);
+console.log(fresh, total);
