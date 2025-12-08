a1720255ff1fca09030b41956de3b9201de64853
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-12-09 00:20:55 +0100

    Add 2025/07

---
+++ 2025/06.ts
@@ -0,0 +1,44 @@
+import $ from '../in.mjs';
+import 'es-toolkit';
+// import _ from 'lodash';
+import 'core-js/actual/iterator/index.js';
+// --- browser devtools cutoff ---
+const t = $('IN/06').textContent.trim().split('\n');
+
+const sum = (a: number, b: number) => a + b;
+const mul = (a: number, b: number) => a * b;
+const ops = t.pop()!.split(/\s+/)
+    .map((o) => o === '+' ? sum : mul);
+
+const rows = t.map(line =>
+    [...line.matchAll(/\d+/g)]
+        .map(Number)
+);
+
+
+const dimIndices = Array.from(Array(rows.length).keys());
+const rowVectors = rows[0].map((_, c) =>
+    dimIndices.map((_, r) => rows[r][c]));
+
+function solveProblems(vectors: number[][], ops: ((a: number, b: number) => number)[]) {
+    const problems = vectors.map((v, i) => v.reduce(ops[i]));
+    return problems.reduce((a, b) => a + b, 0);
+}
+
+
+console.log(solveProblems(rowVectors, ops));
+
+const columns = t[0].split('')
+    .map((_, c) =>
+        dimIndices.map((_, r) => t[r][c]).join('')
+    );
+
+let colVectors: number[][] = [[]];
+for (const column of columns) {
+    if (column.trim() === '') {
+        colVectors.push([]);
+    } else
+        colVectors[colVectors.length - 1].push(+column);
+}
+
+console.log(solveProblems(colVectors, ops));
