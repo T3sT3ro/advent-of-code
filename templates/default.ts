9fb950069b526ab34f219ad947303ea8f6ba71e6
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-12-12 19:48:00 +0100

    Update TS template for easier browser-node cross compat

--- templates/default.ts
+++ templates/default.ts
@@ -2,7 +2,6 @@ import $ from '../in.mjs';
 import 'es-toolkit';
 // import _ from 'lodash';
 import 'core-js/actual/iterator/index.js';
-var inputFile = 'IN/<<<DAY>>>';
 // --- browser devtools cutoff ---
-var inputFile = inputFile ?? 'html';
-const t = $(inputFile).textContent.trim().split('\n');
+const inputFile = 'IN/<<<DAY>>>';
+const t = $(globalThis.window ? 'html' : inputFile).textContent.trim().split('\n');
